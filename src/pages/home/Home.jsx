8c53beaafade753074dba2e2190653f00ba85d0d
felipesanma <felipesmsoto>
2022-07-29 11:16:52 -0400

    auth added and with good design

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,28 +1,28 @@
+import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
 import awsExports from '../../aws-exports';
+import components from '../../components/Authstyles';
 import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Widget from '../../components/widget/Widget';
 import './home.scss';
-
-
 Amplify.configure(awsExports);
 
 const Home = ({ wgType }) => {
   console.log(wgType);
   return (
-    //<Authenticator components={components}>
-    <div className="home">
-      <Sidebar />
-      <div className="homeContainer">
-        <Navbar />
-        <div className='widgets'>
-          <Widget type={wgType} />
+    <Authenticator components={components}>
+      <div className="home">
+        <Sidebar />
+        <div className="homeContainer">
+          <Navbar />
+          <div className='widgets'>
+            <Widget type={wgType} />
+          </div>
         </div>
       </div>
-    </div>
-    //</Authenticator>
+    </Authenticator>
   )
 }
 
