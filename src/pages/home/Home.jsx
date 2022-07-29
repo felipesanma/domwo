b8a2c772a1d338ad5ff99123ff2214c6208818d5
felipesanma <felipesmsoto>
2022-07-29 08:01:56 -0400

    layout auth change

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,52 @@
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, Image, Text, translations, useTheme, View } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { Amplify } from 'aws-amplify';
+import { Amplify, I18n } from 'aws-amplify';
 import awsExports from '../../aws-exports';
 import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Widget from '../../components/widget/Widget';
+import domwoLogo from './grayscale_transparent_nobuffer.png';
 import './home.scss';
+I18n.putVocabularies(translations);
+I18n.setLanguage('es');
 
+I18n.putVocabulariesForLanguage('es', {
+  'Send code': 'Enviar código'
+});
+
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.small}>
+        <Image
+          alt="Domwo logo"
+          src={domwoLogo}
+        />
+      </View>
+    );
+  },
+
+  Footer() {
+    const { tokens } = useTheme();
+
+    return (
+      <View textAlign="center" padding={tokens.space.large}>
+        <Text color={tokens.colors.neutral[80]}>
+          &copy; Todos los derechos reservados
+        </Text>
+      </View>
+    );
+  }
+}
 
 Amplify.configure(awsExports);
 
 const Home = ({ wgType }) => {
   console.log(wgType);
   return (
-    <Authenticator>
+    <Authenticator components={components}>
       <div className="home">
         <Sidebar />
         <div className="homeContainer">
